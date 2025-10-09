05a167c09103fd91664171f0e291d620fca468c8
lucassoares02 <lucas.soares02@hotmail.com>
2025-10-09 14:46:14 -0300

    remove ssl tag

--- db.js
+++ db.js
@@ -1,9 +1,10 @@
-const { Pool } = require('pg');
-require('dotenv').config();
+const { Pool } = require("pg");
+require("dotenv").config();
 
 const pool = new Pool({
-    connectionString: process.env.POSTGRESQL_EXTERNAL_URL,
-    ssl: { rejectUnauthorized: false }, // para Render
+  ssl: false,
+  connectionString: process.env.POSTGRESQL_EXTERNAL_URL,
+  // ssl: { rejectUnauthorized: false }, // para Render
 });
 
 module.exports = pool;

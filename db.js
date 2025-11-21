04d3bcba328b65e47beae4be70640d6e9dd1e77b
lucassoares02 <lucas.soares02@hotmail.com>
2025-11-21 14:56:40 -0300

    add ssl false

--- db.js
+++ db.js
@@ -2,9 +2,9 @@ const { Pool } = require("pg");
 require("dotenv").config();
 
 const pool = new Pool({
-  // ssl: false,
+  ssl: false,
   connectionString: process.env.POSTGRESQL_EXTERNAL_URL,
-  ssl: { rejectUnauthorized: false }, // para Render
+  // ssl: { rejectUnauthorized: false }, // para Render
 });
 
 module.exports = pool;
